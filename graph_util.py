61f48a8a705a2e3f264361a6633db07e09a1f1cf
Aadhithya Ramesh <aadhi@minance.com>
2018-04-12 23:28:23 +0530

    Added find_child (incomplete) and changes to make_graph and Graph class

--- graph_util.py
+++ graph_util.py
@@ -1,4 +1,6 @@
 import pandas as pd
+import re
+
 class Node:
 	def __init__(self,_question="",_keywords=[], _children=[], _is_leaf=False):
 		self.question = _questions
@@ -6,12 +8,19 @@ class Node:
 		self.children = _children
 		self.is_leaf = _is_leaf
 
+	def find_child(self, text):
+
+		text = re.sub('[^0-9a-zA-Z]+', ' ', s)
+		words = text.split()
+		for child in self.children:
+			keywords.append(child.keywords)
+		for word in words:
+
 
 class Graph:
-	def __init__(self,_nodes):
+	def __init__(self,_nodes = []):
 		self.nodes = _nodes
 
 	def make_graph(self,filename):
 		#Reads file with _ separated values and creates graph
-		df = pd.read_csv("knowledgebase.txt",sep = "_")
-		
+		df = pd.read_csv(filename,sep = "_")
