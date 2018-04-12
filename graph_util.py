b5cb5ed648f0c62247df02dd531856a9675ad715
Abhiramon <Abhiramon.R@iiitb.org>
2018-04-12 22:24:19 +0530

    Created Node and Graph classes

--- graph_util.py
+++ graph_util.py
@@ -1,6 +1,17 @@
-class Graph:
-	def __init__(self,_questions,_keywords):
+import pandas as pd
+class Node:
+	def __init__(self,_question="",_keywords=[], _children=[], _is_leaf=False):
 		self.question = _questions
 		self.keywords = _keywords
+		self.children = _children
+		self.is_leaf = _is_leaf
 
-	
+
+class Graph:
+	def __init__(self,_nodes):
+		self.nodes = _nodes
+
+	def make_graph(self,filename):
+		#Reads file with _ separated values and creates graph
+		df = pd.read_csv("knowledgebase.txt",sep = "_")
+		
