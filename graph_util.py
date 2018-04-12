282ab4be9852099196ebf222045d81d222666721
Abhiramon <Abhiramon.R@iiitb.org>
2018-04-13 00:14:01 +0530

    Working: Max num word matching, test graph

--- graph_util.py
+++ graph_util.py
@@ -3,23 +3,42 @@ import re
 
 class Node:
 	def __init__(self,_question="",_keywords=[], _children=[], _is_leaf=False):
-		self.question = _questions
+		self.question = _question
 		self.keywords = _keywords
 		self.children = _children
 		self.is_leaf = _is_leaf
 
 	def find_child(self, text):
 
-		text = re.sub('[^0-9a-zA-Z]+', ' ', s)
-		words = text.split()
-		for child in self.children:
-			keywords.append(child.keywords)
-		for word in words:
+		# text = re.sub('[^0-9a-zA-Z]+', ' ', s)
+		# words = text.split()
+		# for child in self.children:
+		# 	keywords.append(child.keywords)
+		# for word in words:
+
+		max_child = 0
+		max_matches = 0
+		status = True
+		text_words = text.split()
+		for child in self.children :
+			count = 0
+			for word in child.keywords:
+				if word in text_words:
+					count+=1
+			if count > max_matches:
+				max_matches = count
+				max_child = child
+		if max_matches==0:
+			status = False
+
+		return max_child, status
+
 
 
 class Graph:
 	def __init__(self,_nodes = []):
 		self.nodes = _nodes
+		self.root_index = 0
 
 	def make_graph(self,filename):
 		#Reads file with _ separated values and creates graph
--- ui.py
+++ ui.py
@@ -1,25 +1,42 @@
 from graph_util import *
 
 def main():
-	queryBot()
+	# graph = Graph()
+	# graph.make_graph("knowledgebase.txt")
+	graph = testGraph()
+	queryBot(graph)
 
+def testGraph():
+	p1 = Node(_question = "The accused can be sentenced to 2 months in prison.",_is_leaf =True)
+	p2 = Node(_question = "The accused can be sentenced to 5 months in prison.",_is_leaf =True)
+	nc1 = Node ("Is your case related to literature copyright infringment?", ["book","paper","copied", "infringment"],[p1])
+	nc2 = Node ("Is your case related to music copyright infringment?",["music", "copied", "track","record"],[p2])
+	n1  = Node("Does your case fall into copyright cases?",["a","b"], [nc1,nc2])
+	return Graph([n1,nc1,nc2,p1,p2])
 
-
-def queryBot():
+def queryBot(graph):
 	print "Welcome to Legal solutions !!\n"
-	graph = Graph()
-	graph.make_graph("knowledgebase.txt")
-	node = graph.root
-	while (!node.is_leaf){
+	node = graph.nodes[graph.root_index]
+	while (node.is_leaf == False):
 		print(node.question)
 		res = raw_input()
-		if (res == "yes"){
-			
-		}
-		else {
-			print "Sorry we can't help you with that."
-		}
-	}
+		if (res == "yes"):
+			if (node.children[0].is_leaf == True):
+				node = node.children[0]
+				break
+			print "Describe your problem further in the above category."
+			description = raw_input()
+			child_node,status = node.find_child(description)
+			if (status):
+				node = child_node
+		
+		elif (res == "no"):	
+			print "Couldn't find a match."
+		else :
+			print "enter yes or no."
+	
+	if (node.is_leaf):
+		print node.question
 
 
 
